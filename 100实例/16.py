1b0588f0c0795518635456cd64749e95b7a8a0a3
hoilam <43164690@qq.com>
2017-05-08 22:43:31 +0800

    100算法实例

---
+++ 100实例/16.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+
+import datetime
+
+if __name__ == '__main__':
+	
+	# 输出今日日期，格式为 dd/mm/yyyy .更多选项可以查看strftime()方法
+	print(datetime.date.today().strftime('%d/%m/%Y'))
+
+	# 创建日期对象
+	miyazakiBirthDate = datetime.date(1941,1,5)
+
+	print(miyazakiBirthDate.strftime('%d/%m/%Y'))
+
+	# 日期算术运算
+	miyazakiBirthNextDay = miyazakiBirthDate + datetime.timedelta(days=1)
+	
+	print(miyazakiBirthNextDay.strftime('%d/%m/%Y'))
